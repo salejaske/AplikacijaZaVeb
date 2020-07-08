5c3ff8bad78bc432867d86d85c7fd349515b1f9e
Aleksandar Jasarevic <jasarevic977@gmail.com>
2020-07-08 23:23:39 +0200

    Implement upload error responses throigh the API

--- src/controllers/api/article.controller.ts
+++ src/controllers/api/article.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Param, UseInterceptors, UploadedFile } from "@nestjs/common";
+import { Controller, Post, Body, Param, UseInterceptors, UploadedFile, Req } from "@nestjs/common";
 import { Crud } from '@nestjsx/crud'
 import { ArticleService } from "src/services/artricle/article.service";
 import { Article } from "entities/article.entity";
@@ -81,11 +81,13 @@ export class ArticleController{
             }),
             fileFilter: (req, file, callback) => {
                 if(!file.originalname.match(/\.(jpg|png)$/)){
-                    callback(new Error('Bad file extensions!'), false);
+                    req.fileFilterError = 'Bad file extension!';
+                    callback(null, false);
                     return
                 }
                 if(!(file.mimetype.includes('jpeg') || file.mimetype.includes('png'))){
-                    callback(new Error('Bad file content!'), false);
+                    req.fileFilterError = 'Bad file content!';
+                    callback(null, false);
                     return
                 }
 
@@ -93,11 +95,22 @@ export class ArticleController{
             },
             limits: {
                 files: 1,
-                fieldSize: StorageConfig.photoMaxFileSize,
+                fileSize: StorageConfig.photoMaxFileSize,
             },
         })
     )
-    async uploadPhoto(@Param('id') articleId: number, @UploadedFile() photo): Promise <ApiResponse | Photo> {
+    async uploadPhoto(
+        @Param('id') articleId: number, 
+        @UploadedFile() photo, 
+        @Req() req
+        ): Promise <ApiResponse | Photo> {
+
+        if(req.fileFilterError) {
+            return new ApiResponse('error', -4002, req.fileFilterError);
+        }
+        if(!photo) {
+            return new ApiResponse('error', -4002, 'File not uploaded!');
+        }
 
 
         const newPhoto: Photo = new Photo();
