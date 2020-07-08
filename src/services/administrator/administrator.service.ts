a9b74f7daeb0dca86d0e1febcc34739447097661
Aleksandar Jasarevic <jasarevic977@gmail.com>
2020-07-08 03:10:33 +0200

    Implament an global API response mechanism

--- src/services/administrator/administrator.service.ts
+++ src/services/administrator/administrator.service.ts
@@ -4,6 +4,8 @@ import { Administrator } from 'entities/administrator.entity';
 import { Repository } from 'typeorm';
 import { AddAdministratorDto } from 'src/dtos/administrator/add.administrator.dto';
 import { EditAdministratorDto } from 'src/dtos/administrator/edit.administrator.dto';
+import { ApiResponse } from 'src/misc/api.response.class';
+import { resolve } from 'path';
 
 
 @Injectable()
@@ -23,28 +25,41 @@ export class AdministratorService {
 
     }
 
-    add(data: AddAdministratorDto): Promise<Administrator> {
+    add(data: AddAdministratorDto): Promise<Administrator | ApiResponse> {
         const crypto = require('crypto');
         const passwordHash = crypto.createHash('sha512'); 
         passwordHash.update(data.password);
 
-        const passwordHashString = passwordHash.digest('hex').topUpperCase();
+        const passwordHashString = passwordHash.digest('hex').toUpperCase();
 
         let newAdmin: Administrator = new Administrator();
         newAdmin.username = data.username;
         newAdmin.passwordHash = passwordHashString;
 
-        return this.administrator.save(newAdmin);
+        return new Promise((resolve)=>{
+            this.administrator.save(newAdmin)
+            .then(data => resolve(data))
+            .catch(error => {
+                const response: ApiResponse = new ApiResponse("error", -1001);
+                resolve(response);
+            });
+        });
 
     }
 
-    async editById(id: number, data: EditAdministratorDto): Promise<Administrator> {
+    async editById(id: number, data: EditAdministratorDto): Promise<Administrator | ApiResponse> {
         let admin: Administrator = await this.administrator.findOne(id);
+
+        if(admin === undefined){
+            return new Promise((resolve) =>{
+                resolve(new ApiResponse("error", -1002));
+            });
+        }
 
         const crypto = require('crypto');
         const passwordHash = crypto.createHash('sha512'); 
         passwordHash.update(data.password);
-        const passwordHashString = passwordHash.digest('hex').topUpperCase();
+        const passwordHashString = passwordHash.digest('hex').toUpperCase();
 
         admin.passwordHash = passwordHashString;
 
