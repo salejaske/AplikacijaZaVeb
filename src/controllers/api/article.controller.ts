aca56f27616747bb9f7ccf0122c4dcd916d8f9c5
Aleksandar Jasarevic <jasarevic977@gmail.com>
2020-07-09 03:38:00 +0200

    Implement a mechanism to delete photos

--- src/controllers/api/article.controller.ts
+++ src/controllers/api/article.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Param, UseInterceptors, UploadedFile, Req } from "@nestjs/common";
+import { Controller, Post, Body, Param, UseInterceptors, UploadedFile, Req, Delete } from "@nestjs/common";
 import { Crud } from '@nestjsx/crud'
 import { ArticleService } from "src/services/artricle/article.service";
 import { Article } from "src/entities/article.entity";
@@ -164,4 +164,33 @@ export class ArticleController{
             .toFile(destinationFilePath);
 
     }
+
+    @Delete(':articleId/deletePhoto/:photoId')
+    public async deletePhoto(
+        @Param('articleId') articleId: number,
+        @Param('photoId') photoId: number,){
+            const photo = await this.photoService.findOne({
+                articleId: articleId,
+                photoId: photoId
+            });
+
+            if (!photo) {
+            return new ApiResponse("error", -4004, 'Photo not found.');
+            }
+            try{
+            fs.unlinkSync(StorageConfig.photo.destination + photo.imagePath);
+            fs.unlinkSync(StorageConfig.photo.destination +
+                          StorageConfig.photo.resize.thumb.directory +
+                          photo.imagePath);
+            fs.unlinkSync(StorageConfig.photo.destination +
+                          StorageConfig.photo.resize.small.directory +
+                          photo.imagePath);
+            }catch (e) {  }
+
+            const deleteResult = await this.photoService.deleteById(photoId);  
+            if (deleteResult.affected === 0) {
+                return new ApiResponse("error", -4004, 'Photo not found.');
+            }         
+            return new ApiResponse("ok", 0, 'One photo has been deleted.');    
+        }
 }
